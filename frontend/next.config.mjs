8c7c2933ea1d71b0f09015c1d0b2444903e8549f
Conall Heffernan <conallh1234@hotmail.com>
2025-06-09 17:46:42 +0800

    Frontend now rendering

---
+++ frontend/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['images.pexels.com'], // Add external domains you want to allow
+    },
+  };
+  
+  export default nextConfig;
+  
